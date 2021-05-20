10dad5525764a1aef66f27743c08a27bab1f486f
Akiyoshi7922 <1603akiyoshi2045kojima@gmail.com>
2021-05-20 16:05:37 +0900

    テストchordスタート！

--- app/javascript/future_price.js
+++ app/javascript/future_price.js
@@ -1,4 +1,11 @@
 window.addEventListener('load', () => {
   console.log("OK");
-});
-const priceInput = document.getElementById("");
+
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
+  });
+  })
