878d81d888f3ed2f932ff1e87c68b9f21756d91e
Akiyoshi7922 <1603akiyoshi2045kojima@gmail.com>
2021-05-30 16:18:49 +0900

    テストコードに取り掛かります！

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,26 @@
 class OrdersController < ApplicationController
+  before_action :set_order_addres, only: [:index, :create]
   def index
-    @order = Order.new
+    @order_addres = OrderAddres.new
   end
 
   def create
-    @order = Order.new(order_params)
-    if @order.valid?
-      @order.save
-      return redirect_to root_path
+    #  binding.pry
+    @order_addres = OrderAddres.new(order_params)
+    if @order_addres.valid?
+       @order_addres.save
+       return redirect_to root_path
     else
       render 'index'
+    end
   end
-
   private
 
   def order_params
-    params.require(:order).permit(:price)
+    params.permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number)
   end
 
+  def set_order_addres
+    @order_item = Item.find(params[:item_id])
+  end
 end
---
+++ app/helpers/donations_controllers_helper.rb
@@ -0,0 +1,2 @@
+module DonationsControllersHelper
+end
